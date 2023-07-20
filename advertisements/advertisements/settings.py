ef4dea1c0236dcf38465b074f54fa762ad3c3579
daniil2468 <danilvpi3@gmail.com>
2023-07-20 20:27:11 +0300

    задание4

--- advertisements/advertisements/settings.py
+++ advertisements/advertisements/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'advertisements.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [Path.joinpath(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,5 +123,9 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+STATICFILES_DIRS = [
+    Path.joinpath(BASE_DIR, 'static')
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
